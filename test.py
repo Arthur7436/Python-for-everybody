6de964f526ecbcf7baae50129cce7dbbce7220e5
arthur <arthur.thai479@gmail.com>
2024-03-29 17:05:40 +1100

    update

--- test.py
+++ test.py
@@ -1,15 +1,15 @@
-public class Solution {
-    public bool IsValid(string s) {
-        string[] options = {"()","{}", "[]"} ;
+# public class Solution {
+#     public bool IsValid(string s) {
+#         string[] options = {"()","{}", "[]"} ;
 
-        foreach(string option in options){
-            if(s.Contains(option)){
-                return true;
-            }
-            else{
-                return false;
-            }
-        }
-        return false;
-    }
-}
+#         foreach(string option in options){
+#             if(s.Contains(option)){
+#                 return true;
+#             }
+#             else{
+#                 return false;
+#             }
+#         }
+#         return false;
+#     }
+# }
